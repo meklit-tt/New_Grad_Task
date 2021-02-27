aa331dbdf76126e1ba4a9ba3a37608059c0562b0
meklit <meklit_teshome@diveintocode.jp>
2021-02-27 10:47:09 +0900

    bootstrap added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  private
+   def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :description)
+   end
 end
