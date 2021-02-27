0f193f01f66886d17b2d0c9188e10c677dc7a77a
meklit <meklit_teshome@diveintocode.jp>
2021-02-27 12:23:53 +0900

    restaurant view added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,43 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   def index
+      @restaurants = Restaurant.all.order("created_at DESC")
   end
+  def show
 
+  end
   def new
     @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-  end
+    if @restaurant .save
+      redirect_to root_path
+    else
+      render 'new'
+   end
+ end
+ def edit
+ end
 
+ def update
+   if @restaurant.update(restaurant_params)
+     redirect_to restaurant_path(@restaurant)
+   else
+     render 'edit'
+ end
+end
+ def destroy
+   @restaurant.destroy
+   redirect_to root_path
+ end
   private
    def restaurant_params
     params.require(:restaurant).permit(:name, :address, :description)
+   end
+
+   def find_restaurant
+     @restaurant = Restaurant.find(params[:id])
    end
 end
