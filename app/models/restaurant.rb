7ec42d7ad61c751a2bfa0bca2b0775f03508ac64
meklit <meklit_teshome@diveintocode.jp>
2021-02-28 01:28:22 +0900

    style applied

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews
-  has_attached_file :rest_img, styles: { rest_index: "250x300>", rest_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :rest_img, styles: { rest_index: "250x300>", rest_show: "325x400>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :rest_img, content_type: /\Aimage\/.*\z/
 end
