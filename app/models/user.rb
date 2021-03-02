43a2f4418d3670e29977db2e5468ac79911624ed
meklit <meklit_teshome@diveintocode.jp>
2021-03-02 14:34:24 +0900

    validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ has_many :restaurants
 has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-validates :admin,  inclusion: {in: [ true ,  false ]}
+#validates :admin,  inclusion: {in: [ true ,  false ]}
 
 #before_destroy :not_destroy_admin
 
