6d67ef388208c3ee8665483cf2c6fb851332b985
meklit <meklit_teshome@diveintocode.jp>
2021-03-03 20:56:51 +0900

    review solved

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,11 +2,11 @@ class RestaurantsController < ApplicationController
   before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_selet, only: [:new, :edit, :create]
-  before_action :set_search
+
 PER=3
   def index
-       @search = Restaurant.ransack(params[:q])
-       @restaurants = @search.result
+       @q = Restaurant.search(params[:q])
+       @restaurants = @q.result(distinct: true).page(params[:page])
        if params[:category].blank?
        @restaurants = Restaurant.all.order("created_at DESC")
     else
@@ -17,22 +17,22 @@ end
 
   def show
       @restaurants= Restaurant.find(params[:id])
-      if @restaurant.reviews.blank?
+      if @restaurants.reviews.blank?
       @average_review = 0
      else
-     @average_review = @restaurant.reviews.average(:rating).round(2)
+     @average_review = @restaurants.reviews.average(:rating).round(2)
     end
 end
 
   def new
-     @restaurant = current_user.restaurants.build
+     @restaurants = current_user.restaurants.build
      #@categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def create
-     @restaurant = current_user.restaurants.build(restaurant_params)
-     #@restaurant.category_id = params[:category_id]
-  if @restaurant .save
+     @restaurants = current_user.restaurants.build(restaurant_params)
+    # @restaurants.category_id = params[:category_id]
+  if @restaurants .save
      # RestMailer.rest_mailer(@restaurant).deliver
       flash[:notice] = 'New Restaurant added'
       redirect_to root_path
@@ -46,16 +46,16 @@ end
  end
 
  def update
-    @restaurant.category_id = params[:category_id]
-    if @restaurant.update(restaurant_params)
-     redirect_to restaurant_path(@restaurant)
+    #@restaurants.category_id = params[:category_id]
+    if @restaurants.update(restaurant_params)
+     redirect_to restaurant_path(@restaurants)
    else
      render 'edit'
  end
 end
 
  def destroy
-   @restaurant.destroy
+   @restaurants.destroy
    redirect_to root_path
  end
    private
@@ -66,14 +66,11 @@ end
   end
 
   def find_restaurant
-     @restaurant = Restaurant.find(params[:id])
+     @restaurants = Restaurant.find(params[:id])
   end
 
   def set_selet
      @categories = Restaurant.all.map{ |c| [c.name, c.id] }
   end
-  def set_search
-      @search = Restaurant.ransack(params[:q])
-      @restaurant = @search.result
-    end
+
 end
